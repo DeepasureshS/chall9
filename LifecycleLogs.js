99661ba2a7fbd53bf281f936365dd9358d296702
DeepasureshS <230701065@rajalakshmi.edu.in>
2025-10-18 10:47:22 +0530

    Update LifecycleLogs.js

--- LifecycleLogs.js
+++ LifecycleLogs.js
@@ -1,24 +1,27 @@
 import React from 'react';
+
 const LifecycleLogs = ({ logs, CL }) => {
-return (
-<div style={{ marginTop: '30px' }}>
-<h2>Lifecycle Logs</h2>
-<button onClick={CL}>Clear Log</button>
-<div
-style={{
-border: '1px solid #ccc',
-marginTop: '10px',
-padding: '10px',
-minHeight: '100px',
-}}
->
-{logs.length === 0 ? (
-<p>No logs yet</p>
-) : (
-logs.map((l, ind) => <p key={ind}>{l}</p>)
-)}
-</div>
-</div>
-);
+  return (
+    <div style={{ marginTop: '30px' }}>
+      <h2>Lifecycle Logs</h2>
+      <button onClick={CL}>Clear Log</button>
+      <div
+        style={{
+          border: '1px solid #ccc',
+          marginTop: '10px',
+          padding: '10px',
+          minHeight: '100px'
+        }}
+      >
+        {logs.length === 0 ? (
+          <p>No logs yet</p>
+        ) : (
+          logs.map((l, ind) => <p key={ind}>{l}</p>)
+        )}
+      </div>
+    </div>
+  );
 };
+
 export default LifecycleLogs;
+
